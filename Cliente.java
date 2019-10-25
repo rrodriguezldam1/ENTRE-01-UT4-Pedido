ddd87856ce2fe6363d2daae23f75e06bfd9de2b6
Ramón Rodríguez Lucas <rrodriguezldam1@gmail.com>
2019-10-25 10:34:15 +0200

    toString()

--- Cliente.java
+++ Cliente.java
@@ -1,5 +1,4 @@
 
-
 /**
  *  Representa a un cliente que hace un pedido 
  */
@@ -52,10 +51,25 @@ public class Cliente
      * representación textual del cliente
      */
     public String toString() {
-         
-          
+        // String tmp1 = String.format("%10s %-10s","NOMBRE:", nombre);
+        // String tmp2 = String.format("%10s %-10s","DIRECCIÓN:", direccion);
+        // String tmp3 = String.format("%10s %-10s","CIUDAD:", ciudad);
+        // String tmp4 = String.format("%10s %-10s","PROVINCIA:", provincia);
+        // return "\n" + tmp1 + "\n" + tmp2 + "\n" + tmp3 + "\n" + tmp4;
+
+        // return "\n" + String.format("%10s %-10s","NOMBRE:", nombre) + "\n" + String.format("%10s %-10s","DIRECCIÓN:", direccion)
+        // + "\n" + String.format("%10s %-10s","CIUDAD:", ciudad) + "\n" + String.format("%10s %-10s","PROVINCIA:", provincia);
+
+        // return "\n" + String.format("%10s %-10s \n%10s %-10s \n%10s %-10s \n%10s %-10s","NOMBRE:", nombre,
+        // "DIRECCIÓN:", direccion,"CIUDAD:", ciudad,"PROVINCIA:", provincia);
+
+        //---------------------------------------------------------------------------------------------------
+        // return "\n" + String.format("%10s %-10s %10s %-10s %10s %-10s %10s %-10s","NOMBRE:", nombre,
+        // "\nDIRECCIÓN:", direccion,"\nCIUDAD:", ciudad,"\nPROVINCIA:", provincia);
+        
+        return "";
     }
-    
+
     /**
      * 
      */
